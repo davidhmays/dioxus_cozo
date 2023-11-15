53571f29acec07e0d8afb4dfc2476ed61f8feab2
David Mays <david.mays@outlook.com>
2023-11-15 15:40:58 -0700

    add cozo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
+use cozo::*;
 
 fn main() {
     // launch the web app
